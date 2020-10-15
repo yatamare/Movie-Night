f9f269ecbeba04721d6d7561a58d73dc31371088
Hunter McGregor <yatamare@outlook.com>
2020-10-14 19:39:45 -0600

    Movie update
    Movie page now works successfully building a grid of liked movies

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,8 @@ import MenuIcon from '@material-ui/core/Menu';
 // Styles
 const useStyles = makeStyles((theme) => ({
     root: {
-      flexGrow: 1,
-      backgroundColor: '#6f7072',
+        flexGrow: 1,
+        backgroundColor: '#6f7072',
     },
     navBar: {
         backgroundColor: '#313233'
@@ -27,10 +27,10 @@ const useStyles = makeStyles((theme) => ({
     link: {
         textDecoration: 'none',
         color: "white",
-        paddingRight: '40px',
+        paddingRight: '30px',
     },
     linksRow: {
-        paddingRight: '10px',
+        paddingRight: '0px',
     },
     title: {
       flexGrow: 1,
@@ -43,33 +43,30 @@ function App() {
     const classes = useStyles();
 
     return (
-        <React.Fragment>
-        <div>
-            <div className={classes.root}>
-                <Router>
-                    <AppBar position="static" className={classes.navBar}>
-                        <Toolbar>
-                            <div className={classes.title}>
-                                <Typography variant="h5" className={classes.title}>
-                                    <Link to={"/home"} className={classes.link}> Movie Night </Link>
-                                </Typography>
-                            </div>
-                            <div className={classes.linksRow}>
-                                <Typography variant="h6" className={classes.title}>
-                                    <Link to={"/users"} className={classes.link}>Users</Link>
-                                    <Link to={"/movies"} className={classes.link}>Movies</Link>
-                                    <Link to={"/login"} className={classes.link}>Login</Link>
-                                </Typography>
-                            </div>
-                        </Toolbar>
-                    </AppBar>
-                    <Switch>
-                        <Route path="/movies" component={Movies} />
-                    </Switch>
-                </Router>
-            </div>
+        <div className={classes.root}>
+            <Router>
+                <AppBar position="static" className={classes.navBar}>
+                    <Toolbar>
+                        <div className={classes.title}>
+                            <Typography variant="h5" className={classes.title}>
+                                <Link to={"/home"} className={classes.link}> Movie Night </Link>
+                            </Typography>
+                        </div>
+                        <div className={classes.linksRow}>
+                            <Typography variant="h6" className={classes.title}>
+                                <Link to={"/users"} className={classes.link}>Users</Link>
+                                <Link to={"/movies"} className={classes.link}>Movies</Link>
+                                <Link to={"/login"} className={classes.link}>Login</Link>
+                            </Typography>
+                        </div>
+                    </Toolbar>
+                </AppBar>
+                <Switch>
+                    <Route path="" component={Movies} />
+                    <Route path="/movies" component={Movies} />
+                </Switch>
+            </Router>
         </div>
-        </React.Fragment>
     );
 }
 
--- src/Pages/Movies.tsx
+++ src/Pages/Movies.tsx
@@ -6,27 +6,105 @@ import { useQuery } from '@apollo/react-hooks';
 
 // Database
 import { MOVIES_LIKED_BY_USER_QUERY } from '../GraphQL/Queries/movie.queries';
-import { MOVIE_ALL_QUERY } from '../GraphQL/Queries/movie.queries';
 
 // CSS & Material UI
-import '@material-ui/core';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Grid, { GridSpacing } from '@material-ui/core/Grid';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Radio from '@material-ui/core/Radio';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import { BottomNavigation } from '@material-ui/core';
 
+////////////////////////////////////////////
+// Styles
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      height: 300,
+      width: 250,
+      backgroundColor: '#313233',
+    },
+    title: {
+        marginTop: 20,
+        paddingTop: "2px",
+        color: "white",
+        fontSize: 22,
+    },
+    image: {
+        color: "white",
+        textAlign: "center",
+    },
+    description: {
+        color: "white",
+        bottom: "35px",
+    },
+    center: {
+        marginLeft: "auto",
+        marginRight: "auto",
+        textAlign: "center",
+    },
+    remove: {
+        marginLeft: "auto",
+        marginRight: "auto",
+        textAlign: "center",
+    },
+    parent: {
+        position: "relative",
+    }
+  }),
+);
+
+////////////////////////////////////////////
+// App
 function Movies(){
+    const classes = useStyles();
 
     let userId = 0;
     // Grab the required data
     var {data, loading, error} = useQuery(
         MOVIES_LIKED_BY_USER_QUERY, { variables: {user_id: userId}, }
     )
-    let likedMovies = data.Movie;
-    var {data, loading, error} = useQuery( MOVIE_ALL_QUERY )
-    let allMovies = data.Movie;
+    
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error!</p>;
 
     //
     return(
         <div>
-            <p> {likedMovies[0].movie_name} </p>
-            <p> {likedMovies[1].movie_name} </p>
+            <Grid container className={classes.root} spacing={2}>
+            <Grid item xs={12}>
+                <Grid container justify="center" spacing={2}>
+                    {data.Movie.map((movie: any, index: any) => (
+                    <Grid key={index} item>
+                        <Paper elevation={3} className={classes.paper}>
+                            <div className={classes.parent}>
+                                <div className={classes.title}>
+                                    <p className={classes.center}>{movie.movie_name}</p>
+                                </div>
+                                <div className={classes.image}> 
+                                    <p> (image goes here) </p>
+                                </div>
+                                <div className={classes.description}>
+                                    <p className={classes.center}>{movie.movie_description}</p>
+                                </div>
+                                <div className={classes.remove}>
+                                    <Button className={classes.remove} variant="outlined" color="secondary">
+                                        Remove
+                                    </Button>
+                                </div>
+                            </div>
+                        </Paper>
+                    </Grid>
+                ))}
+                </Grid>
+            </Grid>
+            </Grid>
         </div>
     );
 }
